bcc8653b08aa92669e2598d0258212a2887376b0
KostyaDemchenko <kostya.doge@gmail.com>
2024-07-07 15:23:22 +0300

    --added: added UI for all buttons

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,22 @@
 import React from "react";
 import "@/app/globals.scss";
 
+import Button from "@/components/button";
+
 export default function Home() {
-  return <main></main>;
+  return (
+    <main>
+      <Button
+        label='Buy Now'
+        className='btn-primary-50 icon'
+        onClick={() => console.log("test")}
+      />
+      <Button
+        label='Buy Now'
+        className='btn-primary-50 icon'
+        icon='shopping_cart'
+        onClick={() => console.log("test")}
+      />
+    </main>
+  );
 }
