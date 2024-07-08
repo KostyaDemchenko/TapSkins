b8e5acc2746313c5873bd51429b9064449c72178
KostyaDemchenko <kostya.doge@gmail.com>
2024-07-08 16:52:21 +0300

    --added: completed task 17 added lib

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,21 +2,24 @@ import React from "react";
 import "@/app/globals.scss";
 
 import Button from "@/components/button";
+import Components from "@/components/components_test";
 
 export default function Home() {
   return (
     <main>
       <Button
         label='Buy Now'
-        className='btn-primary-50 icon'
+        className='btn-primary-50'
         onClick={() => console.log("test")}
       />
       <Button
         label='Buy Now'
-        className='btn-primary-50 icon'
+        className='btn-primary-50'
         icon='shopping_cart'
         onClick={() => console.log("test")}
       />
+
+      <Components />
     </main>
   );
 }
