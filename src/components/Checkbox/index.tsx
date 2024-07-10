47ef619ef5bdb4202727869c02b9cc46e7a9b1b6
Jpeg <kostya.doge@gmail.com>
2024-07-11 02:45:23 +0300

    --fixed: fixed check box and custom radio button transfered into html elemets

--- src/components/Checkbox/index.tsx
+++ src/components/Checkbox/index.tsx
@@ -1,24 +1,35 @@
-import React from "react";
-
+import React, { useState } from "react";
 import "./style.scss";
 
 interface CustomCheckboxProps {
-  checked: boolean;
-  onChange: () => void;
+  name: string;
+  defaultChecked?: boolean;
 }
 
 const CustomCheckbox: React.FC<CustomCheckboxProps> = ({
-  checked,
-  onChange,
+  name,
+  defaultChecked = false,
 }) => {
+  const [checked, setChecked] = useState(defaultChecked);
+
+  const handleChange = () => {
+    setChecked((prevChecked) => !prevChecked);
+  };
+
   return (
-    <div className='custom-checkbox' onClick={onChange}>
+    <label className='custom-checkbox'>
+      <input
+        type='checkbox'
+        checked={checked}
+        onChange={handleChange}
+        name={name}
+      />
       <div className={`checkbox ${checked ? "checked" : ""}`}>
         {checked && (
           <span className='material-symbols-outlined icon'>check</span>
         )}
       </div>
-    </div>
+    </label>
   );
 };
 
--- src/components/RadioButton/index.tsx
+++ src/components/RadioButton/index.tsx
@@ -1,21 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.scss";
 
 interface CustomRadioButtonProps {
-  selected: boolean;
-  onChange: () => void;
+  name: string;
+  defaultSelected?: boolean;
 }
 
 const CustomRadioButton: React.FC<CustomRadioButtonProps> = ({
-  selected,
-  onChange,
+  name,
+  defaultSelected = false,
 }) => {
+  const [selected, setSelected] = useState(defaultSelected);
+
+  const handleChange = () => {
+    setSelected((prevSelected) => !prevSelected);
+  };
+
   return (
-    <div className='custom-radio-button' onClick={onChange}>
+    <label className='custom-radio-button'>
+      <input
+        type='checkbox'
+        checked={selected}
+        onChange={handleChange}
+        name={name}
+      />
       <div className={`radio-button ${selected ? "selected" : ""}`}>
         <div className='inner-circle'></div>
       </div>
-    </div>
+    </label>
   );
 };
 
