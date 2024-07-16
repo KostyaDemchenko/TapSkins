589ddc18015a2ddd11bf96ef3f7d48eb6a8bfbb2
KostyaDemchenko <kostya.doge@gmail.com>
2024-07-16 15:14:46 +0300

    --updated: added 404 page style

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,5 +1,7 @@
+// "use client";
+
 import React from "react";
-import Nav from "@/src/components/Nav";
+// import Nav from "@/src/components/Nav";
 import Link from "next/link";
 
 import "./globals.scss";
@@ -9,17 +11,17 @@ export default function Custom404() {
   return (
     <>
       <div className='page-not-found'>
-        <div className='description-cintainer'>
-          <h1 className='title'>404 - Page Not Found</h1>
+        <div className='description-container'>
+          <h1 className='title'>404</h1>
           <p className='description'>
             Oops! The page you are looking for does not exist.
           </p>
         </div>
         <Link className='btn-secondary-35' href='/'>
-          Go back home
+          Go backs
         </Link>
       </div>
-      <Nav />s
+      {/* <Nav /> */}
     </>
   );
 }
