fb2720dd27cfb0e91fd741589469099fb57d9d56
KostyaDemchenko <kostya.doge@gmail.com>
2024-08-02 18:46:23 +0300

    --added: added a modal with reward

--- src/components/Referal/index.tsx
+++ src/components/Referal/index.tsx
@@ -1,15 +1,18 @@
-// Default import
 import React, { useEffect, useState } from "react";
 import { ReferalCard } from "@/src/components/Carts";
 import Skeleton from "@mui/material/Skeleton";
-
-// Style import
+import RewardModal from "@/src/components/RevardModal";
 import "./style.scss";
 import { Reward, SuccessDisplay, User } from "@/src/utils/types";
 
-const ReferalList: React.FC<{ user: User, setSuccessClaimedReferal: (e: SuccessDisplay) => void }> = ({ user, setSuccessClaimedReferal }) => {
+const ReferalList: React.FC<{
+  user: User;
+  setSuccessClaimedReferal: (e: SuccessDisplay) => void;
+}> = ({ user, setSuccessClaimedReferal }) => {
   const [rewards, setRewards] = useState<Reward[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
+  const [showModal, setShowModal] = useState<boolean>(false);
+  const [currentReward, setCurrentReward] = useState<Reward | null>(null);
 
   useEffect(() => {
     const fetchRewards = async () => {
@@ -23,7 +26,6 @@ const ReferalList: React.FC<{ user: User, setSuccessClaimedReferal: (e: SuccessD
       } catch (error) {
         console.error("Error fetching rewards:", error);
       } finally {
-        // Задержка перед отключением загрузки
         setTimeout(() => {
           setLoading(false);
         }, 2000);
@@ -33,38 +35,63 @@ const ReferalList: React.FC<{ user: User, setSuccessClaimedReferal: (e: SuccessD
     fetchRewards();
   }, []);
 
+  const handleClaimReward = async (reward: Reward) => {
+    console.log("Claiming reward...");
+    setSuccessClaimedReferal({
+      loading: true,
+      message: "Claiming reward...",
+      success: false,
+    });
+    const response = await user.getReward(reward);
+    setSuccessClaimedReferal({
+      success: response.success,
+      message: response.message,
+      loading: false,
+    });
+    console.log(response);
+    if (response.success) {
+      setCurrentReward(reward);
+      setShowModal(true);
+    }
+  };
+
   return (
     <div className='referal-list'>
       {loading
         ? Array.from(new Array(5)).map((_, index) => (
-          <Skeleton
-            variant='rounded'
-            height={84}
-            animation='wave'
-            sx={{
-              bgcolor: "var(--color-surface)",
-              marginBottom: "5px",
-              width: "100%",
-            }}
-          />
-        ))
+            <Skeleton
+              key={index}
+              variant='rounded'
+              height={84}
+              animation='wave'
+              sx={{
+                bgcolor: "var(--color-surface)",
+                marginBottom: "5px",
+                width: "100%",
+              }}
+            />
+          ))
         : rewards.map((reward) => (
-          <ReferalCard onClickHandle={async () => {
-            console.log("Claiming reward...");
-            setSuccessClaimedReferal({
-              loading: true,
-              message: "Claiming reward...",
-              success: false
-            })
-            const response = await user.getReward(reward);
-            setSuccessClaimedReferal({
-              success: response.success,
-              message: response.message,
-              loading: false
-            });
-            console.log(response);
-          }} key={reward.reward_id} reward={reward} />
-        ))}
+            <ReferalCard
+              onClickHandle={() => handleClaimReward(reward)}
+              key={reward.reward_id}
+              reward={reward}
+            />
+          ))}
+      {currentReward && (
+        <RewardModal
+          isVisible={showModal}
+          onClose={() => setShowModal(false)}
+          rewardAmount={currentReward.reward}
+          rewardName={currentReward.reward_name}
+          rewardType={
+            currentReward.reward_type === "purple_coin" ||
+            currentReward.reward_type === "yellow_coin"
+              ? currentReward.reward_type
+              : "purple_coin"
+          }
+        />
+      )}
     </div>
   );
 };
