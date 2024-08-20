83dac37407b93974068b82a38a3913e9ffea4014
KostyaDemchenko <kostya.doge@gmail.com>
2024-08-20 17:26:48 +0300

    --updated: now daily reward update without page reloading

--- src/components/TasksList/index.tsx
+++ src/components/TasksList/index.tsx
@@ -32,6 +32,9 @@ const TasksList: React.FC<{ user: User }> = ({ user }) => {
   const [loading, setLoading] = useState<boolean>(true);
   const [selectedTask, setSelectedTask] = useState<TaskProps | null>(null);
   const [showModal, setShowModal] = useState<boolean>(false);
+  const [lastDailyClick, setLastDailyClick] = useState<string>(
+    formatDate(user.last_daily_bonus_time_clicked)
+  ); // Добавлено состояние для отслеживания последнего клика
 
   useEffect(() => {
     const fetchTasks = async () => {
@@ -48,14 +51,13 @@ const TasksList: React.FC<{ user: User }> = ({ user }) => {
     };
 
     fetchTasks();
-  }, []);
+  }, [lastDailyClick]); // Обновляем задачи при изменении lastDailyClick
 
   const handleTaskClick = (task: TaskProps) => {
     if (task.platform_type === "Telegram") {
       const formattedLink = task.link_to_join.replace("https://t.me/", "@");
       console.log(`Join Telegram channel: ${formattedLink}`);
     } else {
-      // Используем window.open с параметром "_blank"
       window.open(task.link_to_join, "_blank");
     }
 
@@ -87,9 +89,11 @@ const TasksList: React.FC<{ user: User }> = ({ user }) => {
         ) : (
           user && (
             <DailyReward
-              lastTimeClicked={formatDate(user.last_daily_bonus_time_clicked)}
-              // lastTimeClicked={"05-08-2024 00:00:00"}
+              lastTimeClicked={lastDailyClick} // Используем состояние
               user={user}
+              onRewardClaimed={(newLastClick) =>
+                setLastDailyClick(newLastClick)
+              } // Обновляем состояние при получении награды
             />
           )
         )}
