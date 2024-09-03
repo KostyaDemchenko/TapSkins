55d5e1c7eea80f9022aa1c47987764fe338f6d6e
Dmitry Shumskiy <ejedima260@gmail.com>
2024-09-03 19:29:26 +0300

    --added: history pages looper

--- src/pages/api/order_history/index.tsx
+++ src/pages/api/order_history/index.tsx
@@ -1,6 +1,6 @@
 import { Client } from "@notionhq/client";
 import type { NextApiRequest, NextApiResponse } from "next";
-import type { OrderHistiryDataStructured, RowOrderHistory } from "@/typing";
+import type { MultiSelectOption, OrderHistiryDataStructured, RowOrderHistory, RowSkinStore } from "@/typing";
 
 const notionSecret = process.env.NOTION_SECRET;
 const notionStoreDataBaseld = process.env.NOTION_ORDER_HISTORY_DATABASE_ID;
@@ -8,7 +8,7 @@ const notion = new Client({ auth: notionSecret });
 
 // Извлечение имен из мультиселектов
 function extractMultiSelectNames(options: {
-  multi_select: RowOrderHistory[];
+  multi_select: MultiSelectOption[];
 }): string {
   return options.multi_select.map((option) => option.name).join(", ");
 }
@@ -38,19 +38,31 @@ export default async function handler(
   const { user_id } = req.query; // Получаем user_id из запроса
 
   try {
-    const query = await notion.databases.query({
-      database_id: notionStoreDataBaseld,
-      filter: {
-        property: "user_id",
-        number: {
-          equals: parseInt(user_id as string), // Фильтруем по user_id
-        },
-      },
-    });
+    const allRows: RowOrderHistory[] = [];
+    let hasMore = true;
+    let startCursor: string | undefined = undefined;
 
-    // @ts-ignore
-    const rows = query.results.map((res) => res.properties) as RowSkinStore[];
-    const ordersHistoryDataStructured: OrderHistiryDataStructured[] = rows.map(
+    while (hasMore) {
+      const query = await notion.databases.query({
+        database_id: notionStoreDataBaseld,
+        filter: {
+          property: "user_id",
+          number: {
+            equals: parseInt(user_id as string), // Фильтруем по user_id
+          },
+        },
+        start_cursor: startCursor,
+      });
+
+      // @ts-ignore
+      const rows = query.results.map((res) => res.properties) as RowOrderHistory[];
+      allRows.push(...rows);
+
+      hasMore = query.has_more;
+      startCursor = query.next_cursor!;
+    }
+
+    const ordersHistoryDataStructured: OrderHistiryDataStructured[] = allRows.map(
       (row) => ({
         // for name
         skin_name: row.skin_name.title?.[0]?.text?.content ?? "Default Name",
