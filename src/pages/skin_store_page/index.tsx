f128b45e98bfcbbfee35cf854a59c7a54f44bf3d
Jpeg <kostya.doge@gmail.com>
2024-09-03 15:22:22 +0300

    --fixed: icon fixes

--- src/pages/skin_store_page/index.tsx
+++ src/pages/skin_store_page/index.tsx
@@ -196,11 +196,11 @@ export default function SkinStorePage() {
         />
         <link
           rel='stylesheet'
-          href='https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght@20..48,100..700,0..1,-50..200'
+          href='https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200'
         />
         <link
           rel='stylesheet'
-          href='https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght@20..48,100..700,0..1,-50..200'
+          href='https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200'
         />
         <meta
           name='viewport'
